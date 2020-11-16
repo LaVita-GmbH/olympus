c83388571105d5d715d415f23fbb544a1b2d1482
Manuel Stingl <manuel.stingl@lavita.de>
2020-11-16 17:44:54 +0100

    Add util contextmanager to perform unsafe django methods in async context

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import os
 from pydantic import BaseModel
 from django.db import models
 
@@ -33,3 +34,18 @@ def transfer_to_orm(pydantic_obj: BaseModel, django_obj: models.Model) -> None:
                 raise AttributeError("orm_field not found on %r" % field)
 
             setattr(django_obj, orm_field.field.attname, py_field)
+
+
+class DjangoAllowAsyncUnsafe:
+    def __init__(self):
+        self._django_allow_async_unsafe_before = os.environ.get('DJANGO_ALLOW_ASYNC_UNSAFE')
+
+    def __enter__(self):
+        os.environ['DJANGO_ALLOW_ASYNC_UNSAFE'] = '1'
+
+    def __exit__(self, type, value, traceback):
+        if self._django_allow_async_unsafe_before is None:
+            del os.environ['DJANGO_ALLOW_ASYNC_UNSAFE']
+
+        else:
+            os.environ['DJANGO_ALLOW_ASYNC_UNSAFE'] = self._django_allow_async_unsafe_before
