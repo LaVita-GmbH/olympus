c63ac55c05771d415b1447a271b3db220109d335
Manuel Stingl <manuel.stingl@lavita.de>
2020-12-03 11:26:55 +0100

    Fix LimitOffset

--- utils.py
+++ utils.py
@@ -185,8 +185,8 @@ def validate_object(obj: BaseModel, is_request: bool = True):
 def depends_limit_offset(max_limit: Optional[int] = 1000):
     def get_limit_offset(limit: Optional[int] = Query(None, le=max_limit, ge=1), offset: Optional[int] = Query(None, ge=0)) -> LimitOffset:
         return LimitOffset(
-            limit=limit,
-            offset=offset,
+            limit=limit or max_limit,
+            offset=offset or 0,
         )
 
     return get_limit_offset
