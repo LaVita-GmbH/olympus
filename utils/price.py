2215eef7ffbd7555d28e13c2adcfd18433848935
Manuel Stingl <manuel.stingl@lavita.de>
2021-01-19 11:07:56 +0100

    Refactor calculate gross net

--- utils/price.py
+++ utils/price.py
@@ -2,18 +2,18 @@ from decimal import Decimal
 from typing import Optional
 
 
-def calculate_gross_net(product_price: Decimal, tax_type: str, tax_percentage: Optional[float] = None):
-    tax_rate = 1
+def calculate_gross_net(product_price: Decimal, price_selling_type: str, tax_percentage: Optional[Decimal] = None):
+    tax_rate = Decimal(1)
 
     if tax_percentage:
-        tax_rate = 1 + (tax_percentage / 100)
+        tax_rate = Decimal(1) + (tax_percentage / 100)
 
 
-    if tax_type == 'B2C':
+    if price_selling_type == 'B2C':
         gross = product_price
         net = product_price / tax_rate
 
-    elif tax_type == 'B2B':
+    elif price_selling_type == 'B2B':
         gross = product_price * tax_rate
         net = product_price
 
