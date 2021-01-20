92aabeb63f5ace57401aeaf903e7faa599216d96
Manuel Stingl <manuel.stingl@lavita.de>
2021-01-20 17:19:00 +0100

    Add ContextVar for current access

--- security/jwt.py
+++ security/jwt.py
@@ -1,10 +1,14 @@
 from typing import List, Optional, Tuple
+from contextvars import ContextVar
 from jose import jwt
 from fastapi import HTTPException
 from fastapi.security import SecurityScopes
 from fastapi.security.api_key import APIKeyHeader
 from starlette.requests import Request
 from ..schemas import Error, Access, AccessToken, AccessScope
+
+
+access: ContextVar[Access] = ContextVar('access')
 
 
 class JWTToken(APIKeyHeader):
@@ -34,12 +38,12 @@ class JWTToken(APIKeyHeader):
         if not token:
             return
 
-        access = Access(
+        current_access = Access(
             token=AccessToken(**self.decode_token(token)),
         )
 
         if scopes:
-            audiences = access.token.has_audiences(scopes.scopes)
+            audiences = current_access.token.has_audiences(scopes.scopes)
             if not audiences:
                 raise HTTPException(status_code=403, detail=Error(
                     type='JWTClaimsError',
@@ -49,7 +53,9 @@ class JWTToken(APIKeyHeader):
                 ))
 
             aud_scopes = [AccessScope.from_str(audience) for audience in audiences]
-            access.scopes = aud_scopes
-            access.scope = aud_scopes[0]
+            current_access.scopes = aud_scopes
+            current_access.scope = aud_scopes[0]
 
-        return access
+        access.set(current_access)
+
+        return current_access
