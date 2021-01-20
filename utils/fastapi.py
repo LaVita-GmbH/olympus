8c7bee3a161ce182af780dda26eafb5d05011710
Manuel Stingl <manuel.stingl@lavita.de>
2021-01-20 11:53:21 +0100

    Fix LimitOffset

--- utils/fastapi.py
+++ utils/fastapi.py
@@ -6,9 +6,12 @@ from ..schemas import LimitOffset
 
 def depends_limit_offset(max_limit: Optional[int] = 1000):
     def get_limit_offset(limit: Optional[int] = Query(None, le=max_limit, ge=1), offset: Optional[int] = Query(None, ge=0)) -> LimitOffset:
+        if offset is None:
+            offset = 0
+
         return LimitOffset(
-            limit=limit or max_limit,
-            offset=offset or 0,
+            limit=offset + (limit or max_limit),
+            offset=offset,
         )
 
     return get_limit_offset
