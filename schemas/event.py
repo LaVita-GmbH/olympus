8dd7ea7fdca3c98ca50d75c5390cd0bd164df8ad
Manuel Stingl <manuel.stingl@lavita.de>
2021-03-15 12:08:16 +0100

    Use timezone aware timestamp in EventMetadata

--- schemas/event.py
+++ schemas/event.py
@@ -1,6 +1,7 @@
 from enum import Enum
 import secrets
 from datetime import datetime
+from django.utils import timezone
 from typing import Any, List, Optional
 from pydantic import BaseModel, Field
 
@@ -10,7 +11,7 @@ def default_eid():
 class EventMetadata(BaseModel):
     eid: str = Field(min_length=64, max_length=64, default_factory=default_eid)
     event_type: Optional[str]
-    occurred_at: datetime = Field(default_factory=datetime.now)
+    occurred_at: datetime = Field(default_factory=timezone.now)
     # received_at
     # version
     parent_eids: List[str] = Field([])
