401619f55ca62c924a51c712d9e97132f42b4ae6
Manuel Stingl <manuel.stingl@lavita.de>
2021-05-03 14:43:34 +0200

    Add order_by to Pagination

--- olympus/schemas/query.py
+++ olympus/schemas/query.py
@@ -1,6 +1,8 @@
+from typing import List
 from pydantic import BaseModel
 
 
 class Pagination(BaseModel):
     limit: int
     offset: int
+    order_by: List[str]
--- olympus/utils/fastapi.py
+++ olympus/utils/fastapi.py
@@ -1,17 +1,22 @@
-from typing import Optional
+from typing import List, Optional
 from asgiref.sync import sync_to_async
 from fastapi import Query
 from ..schemas import Pagination
 
 
 def depends_pagination(max_limit: Optional[int] = 1000):
-    def get_pagination(limit: Optional[int] = Query(None, le=max_limit, ge=1), offset: Optional[int] = Query(None, ge=0)) -> Pagination:
+    def get_pagination(
+        limit: Optional[int] = Query(None, le=max_limit, ge=1),
+        offset: Optional[int] = Query(None, ge=0),
+        order_by: List[str] = Query([]),
+    ) -> Pagination:
         if offset is None:
             offset = 0
 
         return Pagination(
             limit=offset + (limit or max_limit),
             offset=offset,
+            order_by=order_by,
         )
 
     return get_pagination
