59e30d23c5766fbbae566c59d3a93fb0699de1ee
Manuel Stingl <manuel.stingl@lavita.de>
2021-05-12 10:58:22 +0200

    Capture message_handler as transaction

--- olympus/handlers/event_consumer.py
+++ olympus/handlers/event_consumer.py
@@ -1,7 +1,26 @@
 from typing import Iterable, Union, Optional, Dict
+import logging
+from functools import wraps
 from event_consumer import message_handler as base_message_handler
 from event_consumer.handlers import DEFAULT_EXCHANGE
 from sentry_sdk.integrations.serverless import serverless_function
+from sentry_sdk import start_transaction, last_event_id
+
+
+_logger = logging.getLogger(__name__)
+
+
+def transaction_captured_function(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        transaction = start_transaction(op='message_handler', name=f'{func.__module__}.{func.__name__}')
+        with transaction:
+            result = func(*args, **kwargs)
+
+        _logger.debug("Logged message handling with trace_id=%s, span_id=%s, id=%s", transaction.trace_id, transaction.span_id, last_event_id())
+        return result
+
+    return wrapper
 
 
 def message_handler(
@@ -16,6 +35,6 @@ def message_handler(
             queue=queue,
             exchange=exchange,
             queue_arguments=queue_arguments,
-        )(serverless_function(func))
+        )(serverless_function(transaction_captured_function(func)))
 
     return decorator
