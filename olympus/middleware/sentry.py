fc05c2e389594e81b5d40851ccda2fdba5d444db
Manuel Stingl <manuel.stingl@lavita.de>
2021-05-12 14:04:18 +0200

    Add X-Flow-ID Response header containing sentry trace_id

--- olympus/middleware/sentry.py
+++ olympus/middleware/sentry.py
@@ -1,4 +1,6 @@
+from starlette.types import Message
 from sentry_sdk.integrations.asgi import SentryAsgiMiddleware as BaseSentryAsgiMiddleware
+from sentry_sdk import Hub
 
 
 class SentryAsgiMiddleware(BaseSentryAsgiMiddleware):
@@ -7,3 +9,13 @@ class SentryAsgiMiddleware(BaseSentryAsgiMiddleware):
             asgi_scope['sentry_event_id'] = event['event_id']
 
         return super().event_processor(event, hint, asgi_scope)
+
+    async def _run_asgi3(self, scope, receive, send):
+        self._send = send
+        return await self._run_app(scope, lambda: self.app(scope, receive, self.send))
+
+    async def send(self, message: Message):
+        message.get('headers', list()).append(
+            (b'X-Flow-ID', Hub.current.scope.transaction.trace_id)
+        )
+        await self._send(message)
