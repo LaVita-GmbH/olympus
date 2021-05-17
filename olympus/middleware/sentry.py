fcff13ea623737bf247ac08d3b17b81419af5a14
Manuel Stingl <manuel.stingl@lavita.de>
2021-05-17 09:20:09 +0200

    Fix RuntimeError and sending wrong response

--- olympus/middleware/sentry.py
+++ olympus/middleware/sentry.py
@@ -11,11 +11,11 @@ class SentryAsgiMiddleware(BaseSentryAsgiMiddleware):
         return super().event_processor(event, hint, asgi_scope)
 
     async def _run_asgi3(self, scope, receive, send):
-        self._send = send
-        return await self._run_app(scope, lambda: self.app(scope, receive, self.send))
+        async def _send(message: Message):
+            message.get('headers', list()).append(
+                (b'X-Flow-ID', Hub.current.scope.transaction.trace_id)
+            )
+            await send(message)
 
-    async def send(self, message: Message):
-        message.get('headers', list()).append(
-            (b'X-Flow-ID', Hub.current.scope.transaction.trace_id)
-        )
-        await self._send(message)
+        return await self._run_app(scope, lambda: self.app(scope, receive, _send))
+
