aed39df2031becac623716009348ba4419c0fc70
Manuel Stingl <manuel.stingl@lavita.de>
2021-05-19 09:17:44 +0200

    Add default order by to Pagination

--- olympus/utils/fastapi.py
+++ olympus/utils/fastapi.py
@@ -4,11 +4,11 @@ from fastapi import Query
 from ..schemas import Pagination
 
 
-def depends_pagination(max_limit: Optional[int] = 1000):
+def depends_pagination(max_limit: Optional[int] = 1000, default_order_by: Optional[List[str]] = None):
     def get_pagination(
         limit: Optional[int] = Query(None, le=max_limit, ge=1),
         offset: Optional[int] = Query(None, ge=0),
-        order_by: List[str] = Query([]),
+        order_by: List[str] = Query(default_order_by or list()),
     ) -> Pagination:
         if offset is None:
             offset = 0
