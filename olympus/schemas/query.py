5f11b51734a6b971c60cc54962daba95f3015047
Manuel Stingl <manuel.stingl@lavita.de>
2021-06-28 13:22:17 +0200

    Pagination handle FieldError and raise as RequestValidationError

--- olympus/schemas/query.py
+++ olympus/schemas/query.py
@@ -1,7 +1,10 @@
 from typing import List, Union
 from pydantic import BaseModel
+from pydantic.error_wrappers import ErrorWrapper
 from django.db.models import Q, QuerySet
 from django.db.models.manager import BaseManager
+from django.core.exceptions import FieldDoesNotExist, FieldError
+from fastapi.exceptions import RequestValidationError
 
 
 class Pagination(BaseModel):
@@ -9,8 +12,14 @@ class Pagination(BaseModel):
     offset: int
     order_by: List[str]
 
-    def query(self, objects: Union[BaseManager, QuerySet], q_filters: Q) -> QuerySet:
+    def query(self, objects: Union[BaseManager, QuerySet], q_filters: Q = Q()) -> QuerySet:
         """
         Filter a given model's BaseManager or pre-filtered Queryset with the given q_filters and apply order_by and offset/limit from the pagination.
         """
-        return objects.filter(q_filters).order_by(*self.order_by)[self.offset:self.limit]
+        try:
+            return objects.filter(q_filters).order_by(*self.order_by)[self.offset:self.limit]
+
+        except (FieldDoesNotExist, FieldError) as error:
+            raise RequestValidationError([
+                ErrorWrapper(error, ("query", "order_by"))
+            ]) from error
