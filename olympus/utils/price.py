4989edb9fb68b7abad76b0c4e5daff667f4dac35
Manuel Stingl <manuel.stingl@lavita.de>
2021-07-08 13:23:38 +0200

    extend calculate_gross_net

--- olympus/utils/price.py
+++ olympus/utils/price.py
@@ -2,18 +2,19 @@ from decimal import Decimal
 from typing import Optional
 
 
-def calculate_gross_net(product_price: Decimal, price_selling_type: str, tax_percentage: Optional[Decimal] = None):
+def calculate_gross_net(product_price: Decimal, price_selling_type: str, tax_percentage: Optional[Decimal] = None, price_includes_tax: Optional[bool] = None):
     tax_rate = Decimal(1)
 
     if tax_percentage:
         tax_rate = Decimal(1) + (tax_percentage / 100)
 
-
-    if price_selling_type == 'B2C':
+    if (price_selling_type == 'B2C' and price_includes_tax in [None, True]) \
+        or (price_selling_type == 'B2B' and price_includes_tax is True):
         gross = product_price
         net = product_price / tax_rate
 
-    elif price_selling_type == 'B2B':
+    elif (price_selling_type == 'B2B' and price_includes_tax in [None, False]) \
+        or (price_selling_type == 'B2C' and price_includes_tax is False):
         gross = product_price * tax_rate
         net = product_price
 
