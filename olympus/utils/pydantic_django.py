a5889d0ce248b58d79a4af23b91b2024cb44ead7
Manuel Stingl <manuel.stingl@lavita.de>
2021-09-03 10:02:00 +0200

    remove ThreadSensitiveContext

--- olympus/utils/pydantic_django.py
+++ olympus/utils/pydantic_django.py
@@ -677,8 +677,7 @@ def orm_object_validator(model: Type[TDjangoModel], value: Union[str, models.Q])
 class DjangoORMBaseModel(BaseModel):
     @classmethod
     async def from_orm(cls, obj: models.Model, filter_submodel: Optional[Mapping[Manager, models.Q]] = None, use_cache: bool = False):
-        async with ThreadSensitiveContext():
-            return await transfer_from_orm(cls, obj, filter_submodel=filter_submodel, use_cache=use_cache)
+        return await transfer_from_orm(cls, obj, filter_submodel=filter_submodel, use_cache=use_cache)
 
     class Config:
         orm_mode = True
