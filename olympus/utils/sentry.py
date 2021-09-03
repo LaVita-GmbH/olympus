04f1e296045886f5c32c148e129e87b937a56df2
Manuel Stingl <manuel.stingl@lavita.de>
2021-09-03 09:57:04 +0200

    Add current thread name to span data

--- olympus/utils/sentry.py
+++ olympus/utils/sentry.py
@@ -1,4 +1,4 @@
-import inspect
+import threading
 from functools import wraps
 from typing import Callable, Optional, Union
 from sentry_sdk import start_span
@@ -18,6 +18,12 @@ def instrument_span(op: str, description: Optional[Union[str, Callable]] = None,
                 description=description(*args, **kwargs) if callable(description) else description,
                 **instrument_kwargs,
             ) as _span:
+                try:
+                    _span.set_data("threading.current_thread", threading.current_thread().getName())
+
+                except Exception:
+                    pass
+
                 span.set(_span)
 
                 return wrapped(*args, **kwargs)
