6004408a4c6f2cb244db344630100b4147c933d8
Manuel Stingl <manuel.stingl@lavita.de>
2021-09-08 15:13:21 +0200

    Add AmountPrecision

--- olympus/schemas/price.py
+++ olympus/schemas/price.py
@@ -5,10 +5,12 @@ from pydantic import BaseModel, validator
 TWO_PLACES = Decimal(10) ** -2
 
 
-class Amount(BaseModel):
+class AmountPrecision(BaseModel):
     gross: Decimal
     net: Decimal
 
+
+class Amount(AmountPrecision):
     @validator('gross', 'net')
     def _round_amount(cls, value: Decimal):
         return value.quantize(TWO_PLACES)
