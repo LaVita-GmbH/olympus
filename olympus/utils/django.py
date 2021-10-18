cff83f41036a57871cc1b14ebd1c2cff2b55b613
Manuel Stingl <manuel.stingl@lavita.de>
2021-10-18 08:46:49 +0200

    Deduplicate calls on_transaction_complete() Add optional arg deduplicate, pass an callable which receives the same arguments as the decorated method and return a hashable object which will be used to deduplicate. Last call wins

--- olympus/utils/django.py
+++ olympus/utils/django.py
@@ -1,11 +1,18 @@
 import os
 import logging
+from contextvars import ContextVar
 from asyncio import Future
 from inspect import CO_COROUTINE
 from functools import wraps
 from typing import Callable, Optional
 from django.db import transaction
 from .asyncio import is_async
+
+
+_logger = logging.getLogger(__name__)
+
+PENDING_TRANSACTION_COMPLETE_OPERATIONS = {}
+pending_transaction_complete_operations: ContextVar[dict] = ContextVar('pending_transaction_complete_operations')
 
 
 class AllowAsyncUnsafe:
@@ -23,7 +30,7 @@ class AllowAsyncUnsafe:
             os.environ['DJANGO_ALLOW_ASYNC_UNSAFE'] = self._django_allow_async_unsafe_before
 
 
-def on_transaction_complete(awaitable: bool = False, callback: Optional[Callable] = None, error_callback: Optional[Callable] = None):
+def on_transaction_complete(awaitable: bool = False, callback: Optional[Callable] = None, error_callback: Optional[Callable] = None, deduplicate: Optional[Callable] = None):
     def wrapper(callable):
         @wraps(callable)
         def wrapped(*args, **kwargs):
@@ -34,8 +41,35 @@ def on_transaction_complete(awaitable: bool = False, callback: Optional[Callable
             elif awaitable and not is_async_:
                 raise AssertionError("Cannot call awaitable from sync context")
 
+            try:
+                pending = pending_transaction_complete_operations.get()
+
+            except LookupError:
+                pending = PENDING_TRANSACTION_COMPLETE_OPERATIONS
+
+            if deduplicate:
+                dedup_id = deduplicate(*args, **kwargs)
+                pending[dedup_id] = callable
+
             future = Future() if awaitable else None
             def call():
+                if deduplicate:
+                    if pending.get(dedup_id) != callable:
+                        _logger.info("Deduplicated call with dedup_id %r to %s", dedup_id, callable)
+                        if awaitable:
+                            future.set_result(None)
+
+                        elif callback:
+                            callback(None)
+
+                        return
+
+                    try:
+                        del pending[dedup_id]
+
+                    except KeyError:
+                        pass
+
                 try:
                     result = callable(*args, **kwargs)
                     if awaitable:
@@ -52,7 +86,7 @@ def on_transaction_complete(awaitable: bool = False, callback: Optional[Callable
                         error_callback(error)
 
                     else:
-                        logging.exception(error, exc_info=True, stack_info=True)
+                        _logger.exception(error, exc_info=True, stack_info=True)
                         raise
 
             if not transaction.get_connection().in_atomic_block:
